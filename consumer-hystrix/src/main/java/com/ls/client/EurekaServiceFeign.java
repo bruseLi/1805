24595415379d56996258f02d1987d16cd914f9c0
bruseLi <2536507877@qq.com>
2018-10-18 15:41:03 +0800

    nihao

---
+++ consumer-hystrix/src/main/java/com/ls/client/EurekaServiceFeign.java
@@ -0,0 +1,13 @@
+package com.ls.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//这个接口相当于把原来的服务提供者项目当成一个Service类
+@FeignClient(value="provider-user")
+public interface EurekaServiceFeign {
+	@RequestMapping(value="/hello/{name}",method=RequestMethod.GET)
+	public String hello(@PathVariable("name") String name);
+}
